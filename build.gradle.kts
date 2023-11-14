113ac73763fdc06eb70697fcb8e608a8d3bb3191
Mark Moelter <Mark.Moelter@integraconnect.com>
2023-11-14 11:37:20 -0500

    Add assertj as dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,16 +11,17 @@ repositories {
 
 dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.28")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.28")
     implementation("org.projectlombok:lombok:1.18.28")
+
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.28")
     testImplementation(platform("org.junit:junit-bom:5.9.2"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-
-
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.2")
+    testImplementation("org.assertj:assertj-core:3.24.2")
 }
 
 
 tasks.test {
-
     useJUnitPlatform()
 }
