f6a3c8909874b82184380fded59c7c095d5ee413
Mark Moelter <Mark.Moelter@integraconnect.com>
2023-11-30 14:48:35 -0500

    Refactor existing methods and add additional methods to Meal.java

--- src/main/java/OopMasterChallenge/Main.java
+++ src/main/java/OopMasterChallenge/Main.java
@@ -3,9 +3,14 @@ package OopMasterChallenge;
 public class Main {
 
   public static void main(String[] args) {
+    Meal meal = new Meal();
+    meal.addBurgerToppings("Bacon", "Cheese", "Mayo");
+    meal.setDrinkSize("large");
+    meal.printItemizedList();
 
-    Burger burger = new Burger("regular", 4.00);
-    burger.addToppings("bacon", "cheese", "mayo");
-    burger.printItem();
+    Meal secondMeal = new Meal("turkey", "7-up", "chile");
+    secondMeal.addBurgerToppings("lettuce", "cheese", "mayo");
+    secondMeal.setDrinkSize("small");
+    secondMeal.printItemizedList();
   }
 }
--- src/main/java/OopMasterChallenge/Meal.java
+++ src/main/java/OopMasterChallenge/Meal.java
@@ -1,13 +1,10 @@
 package OopMasterChallenge;
 
 
-import lombok.AllArgsConstructor;
+import java.util.ArrayList;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
-@AllArgsConstructor
-@ToString
 @Getter
 @Setter
 public class Meal {
@@ -15,52 +12,36 @@ public class Meal {
   /**
    * The burger of the meal.
    */
-  private Burger hamburger;
+  private Burger burger;
 
   /**
    * The drink of the meal.
    */
-  private Drink drink;
+  private Item drink;
 
   /**
    * The side of the meal.
    */
-  private Side side;
+  private Item side;
 
-
+  /**
+   * Creates a meal with a regular burger, a coke, and fries.
+   */
   public Meal() {
-    this(new Burger(3.50), new Drink("Coke", "Small"), new Side("Fries", 1.00));
+    this("regular", "coke", "fries");
   }
 
   /**
-   * Add toppings to the burger.
+   * Create a meal consisting of a burger, a drink, and a side.
    *
-   * @param topping The topping to add to the burger.
+   * @param burgerType The type of burger.
+   * @param drinkType  The type of drink.
+   * @param sideType   The type of side.
    */
-  public void addTopping(Topping topping) {
-    hamburger.addTopping(topping);
-  }
-
-  /**
-   * Change the size of the drink.
-   *
-   * @param newSize The new size of the drink.
-   */
-  public void changeDrinkSize(String newSize) {
-    drink.setSize(newSize);
-  }
-
-  /**
-   * Print out an itemized receipt.
-   */
-  public void printReceipt() {
-    System.out.printf("Burger -> $%.2f%n", hamburger.getBasePrice());
-    for (var topping : hamburger.getToppings()) {
-      System.out.printf("\tTopping: %s -> $%.2f%n", topping.type(), topping.price());
-    }
-    System.out.printf("Drink -> $%.2f%n", drink.getPrice());
-    System.out.printf("Side -> $%.2f%n", side.price());
-    System.out.printf("Total price -> $%.2f%n", getTotalPrice());
+  public Meal(String burgerType, String drinkType, String sideType) {
+    this.burger = new Burger(burgerType, 4.0);
+    this.drink = new Item("drink", drinkType, 1.00);
+    this.side = new Item("side", sideType, 1.50);
   }
 
   /**
@@ -69,12 +50,50 @@ public class Meal {
    * @return The price of the meal.
    */
   public double getTotalPrice() {
-    double total = 0;
+    return side.getAdjustedPrice() + drink.getAdjustedPrice() + burger.getAdjustedPrice();
+  }
 
-    total += hamburger.getTotalPrice();
-    total += drink.getPrice();
-    total += side.price();
+  /**
+   * Print out an itemized receipt.
+   */
+  public void printItemizedList() {
+    burger.printItem();
+    drink.printItem();
+    side.printItem();
+    System.out.println("-".repeat(30));
+    Item.printItem("TOTAL PRICE", getTotalPrice());
+  }
 
-    return total;
+  /**
+   * Add 3 toppings to the burger.
+   *
+   * @param topping1 First topping.
+   * @param topping2 Second topping.
+   * @param topping3 Third topping.
+   */
+  public void addBurgerToppings(String topping1, String topping2, String topping3) {
+    burger.addTopping(topping1);
+    burger.addTopping(topping2);
+    burger.addTopping(topping3);
+  }
+
+  /**
+   * Add toppings to the burger.
+   *
+   * @param toppings An arraylist of toppings to add.
+   */
+  public void addBurgerToppings(ArrayList<String> toppings) {
+    for (String topping : toppings) {
+      burger.addTopping(topping);
+    }
+  }
+
+  /**
+   * Change the size of the drink.
+   *
+   * @param size The new size of the drink.
+   */
+  public void setDrinkSize(String size) {
+    drink.setSize(size);
   }
 }
